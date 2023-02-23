e9f9e2de91c9633bf59be04a73a4ef1cd2643d28
mukohara <mukohara2021@s.okayama-u.ac.jp>
2023-02-23 23:15:18 +0900

    Allow box to play in field

--- src/mario_siblings.rs
+++ src/mario_siblings.rs
@@ -20,7 +20,8 @@ const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
 const BLOCK_SIZE: Vec3 = Vec3::new(10.0, 10.0, 0.0);
 const MARIO_SIZE: Vec3 = Vec3::new(10.0, 20.0, 0.0);
 
-const MARIO_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const BACKGROUND_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
+const MARIO_COLOR: Color = Color::rgb(1.0, 0.0, 0.0);
 
 fn main() {
     App::new()
@@ -32,6 +33,7 @@ fn main() {
             },
             ..default()
         }))
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
         .add_startup_system(make_field)
         .add_startup_system(make_mario)
@@ -66,7 +68,7 @@ fn make_mario(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
+                translation: Vec3::new(0.0, -105.0, 0.0),
                 scale: MARIO_SIZE,
                 ..default()
             },
@@ -97,11 +99,164 @@ fn make_field(mut commands: Commands) {
         ));
         x_coord = x_coord + 10.0;
     }
+
+    // 1段目
+    x_coord = -155.0;
+    while x_coord < -155.0 + UNIT_WIDTH as f32 * 12.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, -60.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    x_coord = 45.0;
+    while x_coord < -155.0 + SCREEN_WIDTH as f32 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, -60.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    // 2段目
+    x_coord = -155.0;
+    while x_coord < -155.0 + UNIT_WIDTH as f32 * 4.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, -10.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    x_coord = -75.0;
+    while x_coord < -155.0 + UNIT_WIDTH as f32 * 24.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, 0.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    x_coord = 125.0;
+    while x_coord < -155.0 + SCREEN_WIDTH as f32 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, -10.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    // 3段目
+    x_coord = -155.0;
+    while x_coord < -155.0 + UNIT_WIDTH as f32 * 14.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, 60.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    x_coord = 25.0;
+    while x_coord < -155.0 + SCREEN_WIDTH as f32 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(x_coord, 60.0, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        x_coord = x_coord + 10.0;
+    }
+
+    // そり立つ壁
+    let mut y_coord = -120.0;
+    while y_coord < -120.0 + SCREEN_HEIGHT as f32 + 100.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(-165.0, y_coord, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        y_coord = y_coord + 10.0;
+    }
+
+    y_coord = -120.0;
+    while y_coord < -120.0 + SCREEN_HEIGHT as f32 + 100.0 {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(165.0, y_coord, 0.0),
+                    scale: BLOCK_SIZE,
+                    ..default()
+                },
+                ..default()
+            },
+            Block,
+        ));
+        y_coord = y_coord + 10.0;
+    }
 }
 
 fn is_on_ground(translation: Vec3) -> bool {
     println!("{}", translation.y);
-    if translation.y <= -105.0 {
+    if translation.y == -105.0
+        || (translation.y == -45.0 && (translation.x < -35.0 || 35.0 < translation.x))
+        || (translation.y == 5.0 && (translation.x < -115.0 || 115.0 < translation.x))
+        || (translation.y == 15.0 && (-85.0 < translation.x && translation.x < 85.0))
+        || (translation.y == 75.0 && (translation.x < -15.0 || 15.0 < translation.x))
+    {
         println!("on the ground!");
         return true;
     } else {
@@ -109,15 +264,6 @@ fn is_on_ground(translation: Vec3) -> bool {
         return false;
     }
 }
-
-// fn is_buried_in_floor(translation: Vec3, velocity: Vec2) -> bool{
-//     let current_x_pos = translation.x;
-//     let current_y_pos = translation.y;
-//     let next_x_pos = translation.x + velocity.x;
-//     let next_y_pos = translation.y + velocity.y;
-//
-//     if next_x_pos
-// }
 
 fn apply_velocity(
     keyboard_input: Res<Input<KeyCode>>,
@@ -127,13 +273,13 @@ fn apply_velocity(
 
     if is_on_ground(mario_transform.translation) {
         if keyboard_input.pressed(KeyCode::Left) {
-            mario_velocity.x = -2.0;
+            mario_velocity.x = -2.5;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            mario_velocity.x = 2.0;
+            mario_velocity.x = 2.5;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            mario_velocity.y = 3.0;
+            mario_velocity.y = 3.5;
         }
         if keyboard_input.pressed(KeyCode::Down) {}
     }
@@ -145,12 +291,6 @@ fn move_mario(mut mario_query: Query<(&mut Velocity, &mut Transform), With<Mario
     let current_y_pos = mario_transform.translation.y;
 
     mario_transform.translation.x = current_x_pos + mario_velocity.x;
-    // if mario_velocity.x < 0{
-    //     mario_velocity.x += 1.0;
-    // }else if 0 < mario_velocity.x{
-    //     mario_velocity.x -= 1.0;
-    // }
-
     mario_transform.translation.y = current_y_pos + mario_velocity.y;
     // Apply gravity!
     mario_velocity.y -= 0.1;
@@ -175,16 +315,20 @@ fn check_for_collisions(
             collision_events.send_default();
 
             match collision {
-                Collision::Left => {}
-                Collision::Right => {}
+                Collision::Left => {
+                    mario_transform.translation.x = transform.translation.x - 10.0;
+                }
+                Collision::Right => {
+                    mario_transform.translation.x = transform.translation.x + 10.0;
+                }
                 Collision::Top => {
                     mario_velocity.x = 0.0;
                     mario_velocity.y = 0.0;
+                    // 地面に埋まるのを防ぐ
                     mario_transform.translation.y = transform.translation.y + 15.0;
                 }
                 Collision::Bottom => {
-                    mario_velocity.x = 0.0;
-                    mario_velocity.y = 0.0;
+                    mario_transform.translation.y = transform.translation.y - 15.0;
                 }
                 Collision::Inside => {}
             }
